f1d235b200dc38219fd4c068341950267f36d075
Sam Zhao <zhao.shengjun@gmail.com>
2018-09-23 22:51:02 +1000

    add welcome page

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,10 @@
 import { configure } from "@storybook/react";
+import { required } from "glamor";
 
 const req = require.context("../src", true, /.stories.js$/);
 
 function loadStories() {
+  require("./welcomeStory");
   req.keys().forEach(file => req(file));
 }
 
---
+++ .storybook/welcomeStory.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+storiesOf("Welcome", module).add("to your new storybook", () => (
+  <h1>Welcome to your new storybook</h1>
+));
