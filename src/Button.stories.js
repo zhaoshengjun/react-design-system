0a80531cfde8841cf6fd24a73f99847a098843e2
Sam Zhao <zhao.shengjun@gmail.com>
2018-09-23 23:04:54 +1000

    info addon

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
 import { Button } from "./Button";
 
 storiesOf("Button", module)
-  .addWithJSX("with background", () => (
-    <Button bg="palegoldenrod">Hello world</Button>
-  ))
+  .addWithJSX(
+    "with background",
+    withInfo(`
+    description of the component
+  `)(() => <Button bg="palegoldenrod">Hello world</Button>)
+  )
   .addWithJSX("with background2", () => (
     <Button bg="green">Hello world</Button>
   ));
