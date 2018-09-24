9354733237493e3b50222aeb28737be081f84b57
Sam Zhao <zhao.shengjun@gmail.com>
2018-09-24 23:29:47 +1000

    add css

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -2,20 +2,10 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
 import { Button } from "./Button";
-import { text } from "@storybook/addon-knobs/react";
+import { text, boolean } from "@storybook/addon-knobs/react";
 
-storiesOf("Button", module)
-  .addWithJSX(
-    "with background",
-    withInfo({
-      styles: {
-        h1: {
-          color: "red"
-        }
-      },
-      text: "  description of the component"
-    })(() => <Button bg="palegoldenrod">Hello world</Button>)
-  )
-  .addWithJSX("with background2", () => (
-    <Button bg={text("bg", "green")}>Hello world</Button>
-  ));
+storiesOf("Button", module).addWithJSX("with background", () => (
+  <Button disabled={boolean("disabled", false)}>
+    {text("text", "Hello world")}
+  </Button>
+));
